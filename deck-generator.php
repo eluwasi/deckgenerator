0750f6bb948fe7603c80ef5cf7fc7a99deb97f46
eluwasi <35965415+eluwasi@users.noreply.github.com>
2024-10-27 17:44:59 +0000

    version update

--- deck-generator.php
+++ deck-generator.php
@@ -3,7 +3,7 @@
 Plugin Name: Deck Generator
 Plugin URI: https://glennorah.co.uk
 Description: Generates investment decks from your WooCommerce data
-Version: 1.0.2  // Increment this from 1.0.1
+Version: 1.0.3  // Increment this from 1.0.1
 Author: Your Name
 Requires at least: 5.8
 Requires PHP: 7.2
